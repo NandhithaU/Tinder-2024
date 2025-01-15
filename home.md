444b239c588f8befe4921890c69a19c0751d7954
Nandhitha <nandhitha@fple.in>
2025-01-16 04:28:54 +0530

    Added signup and all user api

--- home.md
+++ home.md
@@ -1,12 +1,17 @@
-create a free cluster in mongo atlas and get the connection string
-Install mongoose library
-connect the application using the connection string
-call the connectdb function and connect to db before starting an application
-create userSchema and model
-create a post signup api to add data to database
-push some documents using api calls from postman
-Error handling using try catch
+- create a free cluster in mongo atlas and get the connection string
+- Install mongoose library
+- connect the application using the connection string
+- call the connectdb function and connect to db before starting an application
+- create userSchema and model
+- create a post signup api to add data to database
+- push some documents using api calls from postman
+- Error handling using try catch
 
-find the diff btw js object and json
-add express json middleware
-add signup api dynamic to receive data from end user
+- find the diff btw js object and json
+- add express json middleware
+- add signup api dynamic to receive data from end user
+- add get all users api
+- add get specific user api by emailid
+- add patch and delete api 
+- difference between find and findone
+
--- src/app.mjs
+++ src/app.mjs
@@ -15,11 +15,61 @@ app.post('/signup',async (req,res) => {
         await user.save()
         res.status(200).send(`User created successfully`)
     } catch (err) {
-        res.status(400).send (`Error saving the user :${err.message}`)
+        res.status(400).json ({error:`something went wrong :${err.message}`})
     }
 
 })
 
+//Get all users
+app.get('/feed',async (req,res) =>{
+
+    try {
+        const users = await User.find({})
+        res.status(200).send(users)
+    } catch (err) {
+        res.status(400).json ({error:`something went wrong :${err.message}`})
+    }
+
+})
+
+//Get single user
+app.get('/user',async (req,res) =>{
+    const emailId = req.body.emailId
+
+    try {
+        const users = await User.find({emailId})
+        res.status(200).send(users)
+    } catch (err) {
+        res.status(400).json ({error:`something went wrong :${err.message}`})
+    }
+    
+})
+
+//update a user
+app.patch('/user', async (req,res) => {
+    const userId = req.body.userId  
+    const data   = req.body
+    
+    try{
+        await User.findByIdAndUpdate({_id : userId},data)
+        const result = await User.find({_id:userId})
+        res.status(200).json({ message: "User updated successfully", result: result });
+    } catch (err) {
+        res.status(400).json ({error:`something went wrong :${err.message}`})
+    }
+})
+
+//Delete a user
+app.delete('/user', async(req,res)=>{
+    const userId = req.body.userId
+    
+    try {
+        await User.findByIdAndDelete(userId)
+        res.status(200).json({message:"User deleted successfully"})
+    } catch (err) {
+        res.status(400).json ({error:`something went wrong :${err.message}`})
+    }
+})
 
 connectDb.then(()=>{
     console.log(`Database connection successfully established..`)
